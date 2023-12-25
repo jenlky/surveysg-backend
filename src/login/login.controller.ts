47e7f899761cc936454e9199a6c330dd3a6e06fb
Jenssen <jenssen.lee@gmail.com>
2023-12-25 15:52:57 +0800

    Enable cors and set up basic login auth endpoint

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { LoginService } from './login.service';
 
 @Controller()
 export class LoginController {
   constructor(private readonly loginService: LoginService) {}
 
-  @Get('')
-  getHello(): string {
-    return this.loginService.getHello();
+  @Post('/auth')
+  auth(@Body() body): string {
+    console.log('body', body)
+    return this.loginService.auth();
   }
 }
--- src/login/login.service.ts
+++ src/login/login.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class LoginService {
-  getHello(): string {
+  auth(): string {
     return 'Hello World!';
   }
 }
