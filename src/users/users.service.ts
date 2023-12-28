2c5ead6c3eff716bd2ecabde923fa54957d43940
Jenssen <jenssen.lee@gmail.com>
2023-12-28 15:38:27 +0800

    Add userData, save and get one user data functions

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,5 @@
 import { ForbiddenException, Injectable } from '@nestjs/common';
-import { User } from 'src/model/user.model';
+import { User, UserData } from 'src/model/user.model';
 
 @Injectable()
 export class UsersService {
@@ -10,17 +10,44 @@ export class UsersService {
       password: 'johncena',
     },
     {
-      name: 'Maria',
+      name: 'Maria Gonzales',
       email: 'maria@go.gov.sg',
       password: 'mariagonzales',
     },
   ];
+
+  private readonly userData: UserData[] = [
+    {
+      name: 'John Cena',
+      email: 'johncena@gmail.com',
+      agency: 'Central Provident Fund',
+      jobDescription: 'Easy job',
+      termsOfUse: true
+    },
+    {
+      name: 'Maria Gonzales',
+      email: 'mariagonzales@gmail.com',
+      agency: 'ACRA',
+      jobDescription: 'Difficult',
+      termsOfUse: true
+    },
+  ]
 
   async findOne(password: string): Promise<User | undefined> {
     const user = this.users.find(user => user.password === password);
     if (!user) {
       throw new ForbiddenException('User is unauthorised');
     }
+    return user
+  }
+
+  async saveOneUserData(userData: UserData): Promise<UserData | undefined> {
+    this.userData.push(userData)
+    return userData
+  }
+
+  async getOneUserData(name: string): Promise<UserData | undefined> {
+    const user = this.userData.find(user => user.name === name);
     return user
   }
 }
