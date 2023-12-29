eec415dec205a1183d781972647a7b82c87850a6
Jenssen <jenssen.lee@gmail.com>
2023-12-29 15:35:34 +0800

    Add express-session to app middleware

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(
+    session({
+      secret: 'testingforcodingassignment',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
+
   await app.listen(8000);
 }
 bootstrap();
